0d9a27fa9501e3609a7562524829e623877d0d3a
EmyyS2 <miacontapcsenai@gmail.com>
2023-08-22 13:41:09 -0300

    .

--- app/Http/Requests/Petrequest.php
+++ app/Http/Requests/Petrequest.php
@@ -24,7 +24,7 @@ class Petrequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nome'=>'required|max:30|min:5',
+            'nome'=>'required|max:30|min:2',
             'idade'=>'required|max:2|min:1',
             'raca' =>'required|max:20|min:5',
             'requerimentos'=> 'required',
@@ -44,7 +44,7 @@ class Petrequest extends FormRequest
         return [
             'nome.required' => 'O campo nome é obrigatorio',
             'nome.max' => 'O campo nome deve ter no maximo 30 caracteres',
-            'nome.min' => 'O campo nome deve ter no minimo 5 caracteres',
+            'nome.min' => 'O campo nome deve ter no minimo 2 caracteres',
             'idade.required'=> 'O campo idade é obrigatorio',
             'idade.max'=> 'O campo de idade tem que ter 2 caracteres',
             'idade.min'=> 'O campo idade tem que ter no minimo 1 caracter',
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\PetController;
 use App\Models\Clientes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('cadastro/cliente', [ClientesController::class, 'cliente']);
 
-Route::post('cadastro/pets', [ClientesController::class, 'pet']);
+Route::post('cadastro/pets', [PetController::class, 'pet']);
